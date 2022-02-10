e12bc0cdeb4e4a65ad4e8c86fb8514e0da46317b
karais89 <karais89@gmail.com>
2022-02-11 00:31:16 +0900

    [#3] 보편적인 프로그래밍 개념
    - 변수의 가변성

---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+const MAX_POINTS: u32 = 100_000;
+
+fn main() {
+    
+    let mut x = 5;
+    println!("The vale of x is: {}", x);
+    x = 6;
+    println!("The vale of x is: {}", x);
+
+    println!("The const of {}", MAX_POINTS)
+}
