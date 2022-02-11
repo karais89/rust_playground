184100491187d42b3bb371d6489b74d86a419f03
karais89 <jihun3@treenod.com>
2022-02-12 00:22:14 +0900

    [#3] 보편적인 프로그래밍 개념
    - 함수 동작 원리

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,8 @@
+fn five() -> i32 {
+    5
+}
+
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let first = tup.0;
-    let second = tup.1;
-    let third = tup.2;
-    println!("{} {} {}", first, second, third);
+    let x = five();
+    println!("The value of x is: {}", x);
 }
